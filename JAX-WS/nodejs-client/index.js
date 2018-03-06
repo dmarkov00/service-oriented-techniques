834267b56056eb2e888733df89f6d3b1d5324567
Jack D <dmarkov23@gmail.com>
2018-03-06 03:13:51 +0100

    setup of basic nodejs client

--- JAX-WS/nodejs-client/index.js
+++ JAX-WS/nodejs-client/index.js
@@ -2,11 +2,14 @@ let soap = require('soap');
 let url = 'http://localhost:8080/library?wsdl';
 let args = {name: 'value'};
 
+
+console.log("Our Inventory in JSON:");
+console.log();
 soap.createClient(url, (err, client) => {
-    client.getAllBooks(args, (err, result) => {
+    client.getAllBooks({bookName: 'Orange juice'}, (err, result) => {
+
         console.log(result);
     });
 
-    
 
 });
