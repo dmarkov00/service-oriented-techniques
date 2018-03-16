c2c8a7085945966e677e5b1a0a2256c4b71f1f79
Jack D <dmarkov23@gmail.com>
2018-03-16 23:05:07 +0100

    implemented filer by price and genre

--- JAX-RS/library-service/src/library/service/resources/LibraryResources.java
+++ JAX-RS/library-service/src/library/service/resources/LibraryResources.java
@@ -6,6 +6,7 @@ import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,9 +18,9 @@ public class LibraryResources {
 
     public LibraryResources() {
         books = new ArrayList<>(Arrays.asList(
-                new Book("Ghost", "Horror", 22.5),
-                new Book("Pirate", "Comedy", 22.5),
-                new Book("The ship", "Adventure", 22.5),
+                new Book("Ghost", "Horror", 96),
+                new Book("Pirate", "Comedy", 100),
+                new Book("The ship", "Adventure", 21.5),
                 new Book("Dogs", "Drama", 22.5)));
     }
 
@@ -27,10 +28,17 @@ public class LibraryResources {
 
 
     @GET()
-//    @Path("hello")
+    @Path("books")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Book> getAllBooks() {
-        return books;
+    public List<Book> getAllBooks(@QueryParam("genre") String genre, @QueryParam("price") double price) {
+        List<Book> booksResult = this.books;
+        if (genre != null) {
+            booksResult = filterBooksByGenre(genre, booksResult);
+        }
+        if (price != 0.0d) {
+            booksResult = filterBooksByPrice(price, booksResult);
+        }
+        return booksResult;
     }
 
     public void addBook(Book book) {
@@ -50,16 +58,27 @@ public class LibraryResources {
 
     }
 
-    public List<Book> filterBooksByGenre(String genre) {
-        return null;
+
+    private List<Book> filterBooksByGenre(String genre, List<Book> books) {
+
+        List<Book> filteredBooksByGenre = new ArrayList<>();
+        for (Book book : books) {
+            if (book.getGenre().toLowerCase().equals(genre.toLowerCase())) {
+                filteredBooksByGenre.add(book);
+            }
+        }
+        return filteredBooksByGenre;
     }
 
-    public List<Book> filterBooksByPrice(double price) {
-        return null;
-    }
+    private List<Book> filterBooksByPrice(double price, List<Book> books) {
 
-    public List<Book> filerBooksByGenreAndPrice(String genre, double price) {
-        return null;
+        List<Book> filteredBooksByPrice = new ArrayList<>();
+        for (Book book : books) {
+            if (book.getPrice() < price) {
+                filteredBooksByPrice.add(book);
+            }
+        }
+        return filteredBooksByPrice;
     }
 
 
